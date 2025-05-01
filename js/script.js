05c85f7424079f889993a6356d7d38cf52a2e0d1
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-01 17:26:38 +0000

    bruh

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + 
   }
     else if(lengthA === lengthB !== lengthC || lengthA !== lengthB === lengthC || lengthC === lengthA !== lengthB) {
     document.getElementById('answer1').innerHTML =
-      "<p>You calculated towards a Isoceles triangle type</p>" 
+      "<p>You calculated towards a Equilateral triangle type</p>" 
   }
     else if (lengthA !== lengthB !== lengthC && lengthC !== lengthA) {
     document.getElementById('answer1').innerHTML =
